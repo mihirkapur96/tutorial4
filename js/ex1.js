1586b43ae8d13246b10c6c261263a22ddb2a95ae
mihir kapur <mihirkapur96@gmail.com>
2021-02-04 16:28:38 -0800

    Test File

---
+++ js/ex1.js
@@ -0,0 +1,5 @@
+/* Tutorial 4
+   Example 1 JavaScript code
+*/
+
+console.log ("Mihir's Output from Tutorial 4 Example 1");
---
+++ js/ex2.js
@@ -0,0 +1,5 @@
+/* Tutorial 4
+   Example 2 JavaScript Code
+*/
+
+console.log("Mihir's Output from Tutorial 4 Example 2");
